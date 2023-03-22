959665bfdbb810385312899c6df6e51ad7556135
Michal <m.m.dudziak.2@gmail.com>
2023-03-22 21:38:55 +0100

    Added write-objected project, README, updated .gitignore

--- Partitions/Partitions.h
+++ Partitions/Partitions.h
@@ -1,7 +1,39 @@
 #ifndef PROJECTS_PARTITIONS_H
 #define PROJECTS_PARTITIONS_H
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Partition
+{
+private:
+    vector<int> primes;
+    vector<int> combination;
 
 
+public:
+    void write();
 
+    bool isFirst(int k);
+
+    void findPrimes(int number);
+
+    int checkSum();
+
+    int checkMin();
+
+    void end(int choose);
+
+    void addTwo(int n);
+
+    void add(int n, int k);
+
+    void checkCombination(int n, int k);
+
+    void mainAlgorithm(int n, int k);
+
+    void clearContainers();
+};
 
 #endif
---
+++ Partitions/main.cpp
@@ -0,0 +1,84 @@
+/*
+ * C++ Partitions project
+ *
+ * Author: Michal Dudziak
+ */
+
+#include <conio.h>
+#include "Partitions.h"
+
+int main()
+{
+    char button;
+
+    while(true)
+    {
+        Partition partition;
+
+        int l, iteration = 0;
+        int n, k;
+
+        vector<int> ns;
+        vector<int> ks;
+
+        cout << "The program called partitions.\n";
+        cout << "Please type amount of single divisions: \n";
+
+        cin >> l;
+
+        cout << "List " << l << " partitions in format : n and k (eg. 7 3)\n";
+        cout << "n - number to decompose\n";
+        cout << "k - prime number at which to begin the split\n\n";
+
+        while (iteration < l)
+        {
+            cin >> n >> k;
+
+            if (n > 0 && k > 0)
+            {
+                ns.push_back(n);
+                ks.push_back(k);
+            }
+            iteration++;
+        }
+
+        iteration = 0;
+
+        while (iteration != l)
+        {
+            if (iteration > l)
+                return 0;
+
+            n = ns[iteration];
+
+            k = ks[iteration];
+
+            if (n > 1 && k > 1)
+            {
+                partition.findPrimes(k);
+
+                partition.mainAlgorithm(n, k);
+
+                partition.clearContainers();
+            }
+
+            iteration++;
+        }
+
+        cout << endl << endl;
+
+        cout << "Press ENTER to make new partitions." << endl;
+        cout << "If you want leave the program, please enter q." << endl;
+
+        do
+        {
+            button = _getch();
+
+        }while (button != 13 && button != 'q');
+
+        if (button == 'q')
+        {
+            return 0;
+        }
+    }
+}
