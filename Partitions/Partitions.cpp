7ebd4f75a06ca0de4dd1c3f76fea255b487168fa
Michal <m.m.dudziak.2@gmail.com>
2023-03-22 23:01:40 +0100

    Added library, changed logic of files

--- Partitions/Partitions.cpp
+++ Partitions/Partitions.cpp
@@ -1,275 +1,82 @@
-#include "Partitions.h"
-#include <iostream>
+// PARTITIONS
+// C++ Language
+// Author: Michal Dudziak
 
-using namespace std;
+#include <conio.h>
+#include "PartitionNumbers.h"
 
-
-void Partition::write()
-/*
- * Writes found combinations on standard terminal output.
- */
+int main()
 {
-    for (int i = 0; i < combination.size(); i++)
-    {
-        cout << combination[i];
+    char button;
 
-        if (i != combination.size() - 1)
+    while(true)
+    {
+        Partition partition;
+
+        int l, iteration = 0;
+        int n, k;
+
+        vector<int> ns;
+        vector<int> ks;
+
+        cout << "The program called partitions.\n";
+        cout << "Please type amount of single divisions: \n";
+
+        cin >> l;
+
+        cout << "List " << l << " partitions in format : n and k (eg. 7 3)\n";
+        cout << "n - number to decompose\n";
+        cout << "k - prime number at which to begin the split\n\n";
+
+        while (iteration < l)
         {
-            cout << "+";
-        }
-    }
-    cout << endl;
-}
+            cin >> n >> k;
 
-bool Partition::isFirst(int k)
-/*
- * Check if the passed argument is prime number.
- *
- * Arguments:
- *    k: number to check
- */
-{
-    if (k < 2)
-        return false;
-
-    for (int i = 2; i < k; i++)
-    {
-        if (k % i == 0)
-        {
-            if (i == k)
-                return true;
-            else
-                return false;
-        }
-    }
-    return true;
-}
-
-void Partition::findPrimes(int number)
-/*
- * Finds all prime numbers to passed argument
- *
- * Arguments:
- *     number: Number to find the prime numbers
- */
-{
-    for (int i = 0; i <= number; i++)
-    {
-        if (isFirst(i))
-        {
-            primes.push_back(i);
-        }
-    }
-}
-
-int Partition::checkSum() // check sum of combination
-/*
- * Checks sum of the combination. The sum can not be greater than the largest number in the division.
- */
-{
-    int sum = 0;
-
-    for (int i : combination)
-    {
-        sum += i;
-    }
-    return sum;
-}
-
-int Partition::checkMin()
-/*
- * Finds min index in combination container
- */
-{
-    int min = combination[0], idx = 0;
-
-    for (int i = 0; i < combination.size(); i++)
-    {
-        if (min > combination[i])
-        {
-            min = combination[i];
-            idx = i;
-        }
-    }
-    return idx;
-}
-
-void Partition::end(int choose) // changing numbers to 2 from given index
-/*
- * Checks edge case and changes all the words in combination to '2'.
- */
-{
-
-    for (int i = choose; i < combination.size(); i++)
-    {
-
-        combination[i] = primes[0];
-    }
-}
-
-void Partition::addTwo(int n)
-/*
- * Adding '2' to container untill sum + 2 <= n
- *
- * Arguments:
- *     n: Parameter to which sum is calculating
- */
-{
-    if (checkSum() + 2 <= n)
-    {
-        while (checkSum() < n && checkSum() + 1 <= n)
-        {
-            combination.push_back(primes[0]);
-        }
-    }
-    else
-        return;
-}
-
-void Partition::add(int n, int k)
-/*
- * Add new numbers to the next words of the current partition.
- *
- * Arguments:
- *     n: number to divide
- *     k: first prime number in partition
- */
-{
-    int number = 0;
-
-    if (combination[combination.size() - 1] < k)
-    {
-        for (int i = 0; i < primes.size(); i++) // assign higher prime number to first min number
-        {
-            if (primes[i] == combination[checkMin()])
+            if (n > 0 && k > 0)
             {
-                number = primes[i + 1];
+                ns.push_back(n);
+                ks.push_back(k);
             }
+            iteration++;
         }
 
-        combination[checkMin()] = number;
+        iteration = 0;
 
-        if (combination.size() != 1 && combination[combination.size() - 1] != number)
+        while (iteration != l)
         {
-            end(checkMin());
+            if (iteration > l)
+                return 0;
+
+            n = ns[iteration];
+
+            k = ks[iteration];
+
+            if (n > 1 && k > 1)
+            {
+                partition.findPrimes(k);
+
+                partition.mainAlgorithm(n, k);
+
+                partition.clearContainers();
+            }
+
+            iteration++;
         }
 
-        if (checkSum() < n)
+        cout << endl << endl;
+
+        cout << "Press ENTER to make new partitions." << endl;
+        cout << "If you want leave the program, please enter q." << endl;
+
+        do
         {
-            addTwo(n);
-        }
-    }
-    else
-    {
-        return;
-    }
-}
+            button = _getch();
 
-void Partition::checkCombination(int n, int k)
-/*
- * Checks combination of partition if it is correct. If correct writes on standard output, if not skip.
- */
-{
-    if (checkSum() > n && combination[combination.size() - 1] > n - k)
-        return;
+        }while (button != 13 && button != 'q');
 
-    if (checkSum() > n)
-    {
-        combination.pop_back();
-        if (checkSum() == n)
-            write();
-        add(n, k);
-        if (checkSum() > n && combination[combination.size() - 1] == primes[primes.size() - 1])
-            return;
-    }
-
-    if (combination[combination.size() - 1] == primes[primes.size() - 1])
-    {
-        if (checkSum() == n)
+        if (button == 'q')
         {
-            write();
+            return 0;
         }
-        return;
     }
-
-    if (checkSum() == n)
-    {
-        write();
-        add(n, k);
-    }
-}
-
-void Partition::mainAlgorithm(int n, int k)
-/*
- * Main method in partitions. Checks many edge cases. Calculates next words in the partition.
- *
- * Arguments:
- *     n: number to divide
- *     k: max prime number in partitions
- */
-{
-    if (!isFirst(k)) // check if k is prime number
-    {
-        return;
-    }
-
-    combination.push_back(k);
-
-    if (n == k)
-    {
-        write();
-        return;
-    }
-
-    addTwo(n);
-
-    if (checkSum() == n)
-    {
-        write();
-        add(n, k);
-        if (primes[primes.size() - 1] == 2)
-            return;
-    }
-
-    while (combination.size() != 1)
-    {
-        if (checkSum() > n && combination[1] >= k && combination.size() < 3 ||
-                checkSum() > n && combination[1] > n - k)
-            return;
-
-        if (checkSum() == n)
-        {
-            checkCombination(n, k);
-        }
-
-        if (combination.size() == 1)
-        {
-            checkCombination(n, k);
-            return;
-        }
-        if (checkSum() > n)
-        {
-            checkCombination(n, k);
-            if (checkSum() > n && checkMin() == 0)
-                return;
-            if (checkSum() > n && combination[checkMin() - 1] >= k && combination[checkMin()] > 3 && combination[
-                                                                                                               checkMin() - 1] != k)
-                return;
-        }
-
-        if (checkSum() < n)
-        {
-            add(n, k);
-
-            checkCombination(n, k);
-        }
-        if (combination[combination.size() - 1] == k)
-            return;
-    }
-}
-
-void Partition::clearContainers()
-{
-    combination.clear();
-    primes.clear();
 }
